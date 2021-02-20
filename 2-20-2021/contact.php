1d98dbc05802f29f4c9ffdc18d452179993c55de
ermal morina <ermalmorina2000@gmail.com>
2021-02-20 18:20:08 +0100

    '

---
+++ 2-20-2021/contact.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Contact Elise</title>
+        <link rel="stylesheet" type="text/css" href="style-contact.css">
+    </head>
+    <body>
+    <script src="validateContact.js"></script>
+    <header>
+        <div class="main">
+            <div class="logo">
+                <img src="logo.png">
+                <div class="links">
+                    <ul>
+                        <li class="active"><a > Contact</a></li>    
+                        <li> <a href="loginRegister.php">Register</a></li>
+                        <li><a href="index.php">Home</a></li>
+                        
+                    </ul>
+                </div>  
+                <form class="register-form ">
+                <div class="login-page">
+                    <div class="form" >
+
+                            <h2 style="font-family: Arial, Helvetica, sans-serif; color: grey;">Insert your info!</h2>
+                            <input id="emaili" type="text" placeholder="email" required >
+                            <h1 id="results"></h1>
+                            <textarea id="textarea" name="text" class="formcontrol" placeholder="message" row="4" required ></textarea>
+                            
+                            <button onclick="validation()">Submit</button>
+                            </div> 
+                        </form>
+                </div>
+    </header>
+
+    </body>
+</html>
---
+++ 2-20-2021/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Shop Elise</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header>
+            <div class="main">
+                <div class="logo">
+                    <img src="logo.png">
+                </div>
+                <ul>
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="loginRegister.php">Login/Register</a></li>
+                    <li><a href="#">About Us</a></li>
+                </ul>
+            <div class="ctg">
+                <h2>Perfumes as weird as you are :)</h2>
+            </div>
+            </div>
+        </header>
+        <div class="products" style=" background-color:grey; border:2px solid black" >
+        <div class="outer">
+            <div class="inner">
+                <img src="product1/parfume1.png" alt="product">
+                
+            </div>
+            <h1>Dior Sauvage</h1>
+                <div >Spray on a bit of Dior Sauvage by Christian Dior before heading out for the night to enjoy the masculine combination of spicy bergamot, peppery patchouli and a cedar dry down that mingles with your natural essence</div>
+                <h2 class="price"><a href="buyProduct.html"> Buy for:170€ </a></h2>
+        </div>
+        <div class="outer">
+            <div class="inner">
+                <img src="product1/parfume2.png" alt="product">
+            </div>
+            <h1>Lorem ipsum </h1>
+                <div >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi odio alias est itaque rem nisi necessitatibus quaerat facilis. Officiis fugiat nemo accusamus, suscipit minima voluptatibus ut iure illum nesciunt magnam.</div>
+                <h2 class="price"><a href="buyProduct.html"> Buy for:100€ </a></h2>
+            </div>
+            <div class="outer">
+            <div class="inner">
+                <img src="product1/parfume3.png" alt="product">
+            </div>
+            <h1>Burberry Touch</h1>
+                <div >This perfume features top and mid notes that are predominantly fruity and floral. The aromas of citrus and rose give Burberry Touch a soft, feminine scent, while the stronger base notes of cedar, vanilla, and oakmoss add a subtle warmth.</div>
+                <h2 class="price"><a href="buyProduct.html"> Buy for:80€ </a></h2>
+        </div>
+        <div class="outer">
+            <div class="inner">
+                <img src="product1/parfume4.png" alt="product">
+            </div>
+            <h1>Invictus</h1>
+                <div >Invictus. Incarnation of a man in all his power. Absolute hero. Blessed by the gods and ready to take on any challenge. Invictus men fragrance embodies the victory. A phenomenal shock between sharp freshness and animal sensuality.</div>
+                <h2 class="price"><a href="buyProduct.html">Buy for:80€ </a></h2>
+        </div>
+        <div class="outer">
+            <div class="inner">
+                <img src="product1/parfume5.png" alt="product">
+            </div>
+            <h1>1Million</h1>
+                <div >1 Million. The scent of insolence. A seductive wake, irresistible. There is never too much gold for 1 Million. A bad boy who views life as BIG. Otherwise, what's the point? Does not obey anyone except his fantasies. Golden rule. And he gets what he wants. In a snap.</div>
+                <h2 class="price"><a href="buyProduct.html">Buy for:75€</a></h2>
+        </div>
+        <div class="outer">
+            <div class="inner">
+                <img src="product1/parfume6.png" alt="product">
+            </div>
+            <h1>D%G Light Blue</h1>
+                <div >D & G Light Blue by Dolce & Gabbana is a fragrance made for the casual woman. Introduced in 2001, this perfume features a subtle scent that will bring you the attention you desire. Its mix of apple, rose, jasmine and musk fragrances gives it a unique scent.</div>
+                <h2 class="price"><a href="buyProduct.html">Buy for:60€</a></h2>
+        </div>
+        </div>
+        
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div>
+        <div class="slideshow-container">
+
+            <div class="mySlides">
+              <q>“Each time we inhale or smell a Perfume, we are all going on our own very personal Perfume journey”</q>
+              <p class="author">-Huib Maat</p>
+            </div>
+            
+            <div class="mySlides">
+              <q>“The beauty of fragrance is that it speaks to your heart…and hopefully someone else's”</q>
+              <p class="author">– Elizabeth Taylor.</p>
+            </div>
+            
+            <div class="mySlides">
+              <q>“The best investments in every man’s life are a custom tailored suit and an expensive cologne. It’s like buying the best first impression.”</q>
+              <p class="author">-Massimo Ferragamo</p>
+            </div>
+            
+            <a class="prev" onclick="plusSlides(-1)">❮</a>
+            <a class="next" onclick="plusSlides(1)">❯</a>
+            
+            </div>
+            
+            <div class="dot-container">
+              <span class="dot" onclick="currentSlide(1)"></span> 
+              <span class="dot" onclick="currentSlide(2)"></span> 
+              <span class="dot" onclick="currentSlide(3)"></span> 
+            </div>
+
+            <div class="social">
+            <img style="width: 150px;" class="logo" src="logo.png" alt="logo">
+            <a href="#"><img src="fblogo.png" alt="facbook"></a>
+            <a href="#"><img src="iglogo.png" alt="facbook"></a> 
+            </div>    
+            </div>
+        <script src="main.js"></script>
+    </body>
+</html>
